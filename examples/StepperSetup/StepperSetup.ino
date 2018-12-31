eb81eb1fb9b51121b9ea7012c81be768d4e60f39
SebisCodes <46286924+SebisCodes@users.noreply.github.com>
2018-12-31 22:11:15 +0100

    Update StepperSetup.ino

--- examples/StepperSetup/StepperSetup.ino
+++ examples/StepperSetup/StepperSetup.ino
@@ -12,7 +12,7 @@ void setup() {
   stepper.init(pin_step, pin_dir, pin_en);
   stepper.setStepsPerRotation(200);
   stepper.setDiameter(60); //mm
-  //stepper.setDiameter(8.0/PI); //For Thread: T8; Pitch:2mm; 4 Threads; 2*4=8mm pitch
+  //stepper.setDiameter(8.0/PI); //for thread: T8; pitch:2mm; 4 threads; 2*4=8mm pitch
   stepper.setDir(1);
   stepper.setFeedrate(0.05f);
   stepper.setAccelration(0.025f);
